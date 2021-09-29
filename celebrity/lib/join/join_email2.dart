e47a586671d7628d279f46b0f2c45efa2d43b03d
dev-hjkim <guswlsapdlf@khu.ac.kr>
2021-09-30 01:38:01 +0900

    connect api

--- celebrity/lib/join/join_email2.dart
+++ celebrity/lib/join/join_email2.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "../utils/http_util.dart";
 
 class JoinEmail2 extends StatefulWidget {
   const JoinEmail2({Key? key}) : super(key: key);
@@ -11,13 +12,18 @@ class _JoinEmail2State extends State<JoinEmail2> {
   late TextEditingController email;
   late TextEditingController code;
 
+  late HttpUtil httpUtil;
+  late Map<String, dynamic> data;
+
   bool showCode = false;
-  bool showNickname = false;
 
   @override
   void initState() {
     email = TextEditingController();
     code = TextEditingController();
+    httpUtil = HttpUtil();
+    data = {};
+    showCode = false;
     super.initState();
   }
 
@@ -25,92 +31,97 @@ class _JoinEmail2State extends State<JoinEmail2> {
   Widget build(BuildContext context) {
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
-          return Scaffold(
-              appBar: AppBar(
-                title: Center(child: Text(
-                    "이메일로 가입할 수 있어요!",
-                    style: TextStyle(color:Colors.black),
-                    textAlign: TextAlign.center)
-                ),
-                leading: IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  color: Colors.black,
-                  icon: Icon(Icons.arrow_back_ios_sharp),
-                ),
-                elevation: 0,
-                backgroundColor: Colors.transparent,
-              ),
-              body: Builder(
-                  builder: (context) => SingleChildScrollView(
+      return Scaffold(
+          appBar: AppBar(
+            title: Center(
+                child: Text("이메일로 가입할 수 있어요!",
+                    style: TextStyle(color: Colors.black),
+                    textAlign: TextAlign.center)),
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              color: Colors.black,
+              icon: Icon(Icons.arrow_back_ios_sharp),
+            ),
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+          ),
+          body: Builder(
+              builder: (context) => SingleChildScrollView(
                       child: Center(
                           child: Column(children: [
-                            Padding(
-                                padding: EdgeInsets.all(20),
-                                child: Container(
-                                    child: Column(
-                                        children: [
-                                          Container(
-                                            color: Color(0xffF4F4F4),
-                                            child:
-                                              Column(
-                                                children: [
+                    Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Container(
+                            child: Column(children: [
+                          Container(
+                              color: Color(0xffF4F4F4),
+                              child: Column(children: [
+                                TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "이메일을 입력해주시겠어요?",
+                                      suffixIcon: IconButton(
+                                          onPressed: () {
+                                            email.clear();
+                                          },
+                                          icon: Icon(Icons.cancel,
+                                              color: Colors.grey))),
+                                  controller: email,
+                                ),
+                              ])),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(data["message"] ?? "",
+                                style: TextStyle(color: Color(0xff6D00B0))),
+                          ),
+                          Visibility(
+                            visible: showCode,
+                            child: Container(
+                                color: Color(0xffF4F4F4),
+                                child: Column(children: [
+                                  TextField(
+                                    decoration: InputDecoration(
+                                        hintText: "인증코드를 입력해주세요!",
+                                        suffixIcon: IconButton(
+                                            onPressed: () {
+                                              code.clear();
+                                            },
+                                            icon: Icon(Icons.cancel,
+                                                color: Colors.grey))),
+                                    controller: code,
+                                  ),
+                                ])),
+                          ),
+                          Row(children: [
+                            Expanded(
+                              child: RaisedButton(
+                                child: Text("다음",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    )),
+                                color: Color(0xffC192DE),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                onPressed: () => {
+                                  setState(() {
+                                    sendMail(email.text);
+                                  })
+                                },
+                              ),
+                            )
+                          ]),
+                        ]))),
+                  ])))));
+    });
+  }
 
-                                                  TextField(
-                                                    decoration: InputDecoration(
-                                                        hintText: "이메일을 입력해주시겠어요?",
-                                                        suffixIcon: IconButton(
-                                                            onPressed: () {
-                                                              email.clear();
-                                                            },
-                                                            icon: Icon(Icons.cancel, color: Colors.grey)
-                                                        )
-                                                    ),
-                                                    controller: email,
-                                                  ),
-                                                ]
-                                              )
-                                          ),
-                                          Text(
-                                            "",
-                                            style: TextStyle(
-                                              color: Color(0xff6D00B0)
-                                            )
-                                          ),
-                                          Row(
-                                            children: [
-                                              Expanded(
-                                                child: RaisedButton(
-                                                  child: Text(
-                                                      "다음",
-                                                      style: TextStyle(
-                                                        color: Colors.white,
-                                                      )
-                                                  ),
-                                                  color: showCode ? Color(0xffC192DE) : Color(0xff6D00B0),
-                                                  shape: RoundedRectangleBorder(
-                                                      borderRadius: BorderRadius.circular(10.0),
-                                                      ),
-                                                  onPressed: () => {
-                                                    setState(() {
-                                                      showCode = true;
-                                                    })
-                                                  },
-                                                ),
-                                              )
-                                            ]
-                                          ),
-                                        ]
-                                    )
-                                )
-                            ),
-                          ])
-                      )
-                  )
-              )
-          );
-        }
-    );
+  Future sendMail(email) async {
+    data = await httpUtil.validateMail(email);
+    setState(() {
+      data = data;
+      showCode = data["success"];
+    });
   }
 }
--- celebrity/lib/utils/http_util.dart
+++ celebrity/lib/utils/http_util.dart
@@ -2,25 +2,31 @@ import "package:http/http.dart" as http;
 import "dart:io";
 import "dart:convert";
 
-class HttpHelper {
+class HttpUtil {
   final String urlBase = "http://192.168.1.16:8080/v1/api/auth";
   final String urlUpcoming = "/upcoming?";
   final String urlLanguage = "&language=ko-KR";
-  final String urlSearchBase = "https://api.themoviedb.org/3/search/movie?api_key=952a21a203ad2415fd13cbaf87e663c1&query=";
+  final String urlSearchBase =
+      "https://api.themoviedb.org/3/search/movie?api_key=952a21a203ad2415fd13cbaf87e663c1&query=";
 
+  Future<Map<String, dynamic>> validateMail(String email) async {
+    final String validateMail = urlBase + "/validate/mail";
 
-  Future<Map<String, String>> resValidateMail() async {
-    final String validateMail = "/validate/mail";
-    http.Response result = await http.post(Uri.parse(validateMail));
+    Map<String, String> resBody = {"username": email};
 
-    if(result.statusCode == HttpStatus.ok) {
-      final jsonResponse = json.decode(result.body);
-      final moviesMap = jsonResponse["results"];
-      return moviesMap;
+    http.Response result = await http.post(
+      Uri.parse(validateMail),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: jsonEncode(resBody),
+    );
+
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(utf8.decode(result.bodyBytes));
+      return jsonResponse;
     } else {
-      return Map();
+      return {};
     }
   }
-
 }
-
